f775bd4c657076edfe3c76e40a019cf0cb0dafc1
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-05 22:44:48 -0500

    Create TrackList component class

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import {Track} from '../Track/Track.js';
+
+export class TrackList extends React.Component {
+  render() {
+    return <div className="TrackList">{/* <!-- render Track components here(via array.map? We'll see....)  --> */}</div>;
+  }
+}
