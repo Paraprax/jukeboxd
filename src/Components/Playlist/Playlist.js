49c4afca017d1ef770c11a598065e3c6611adbc6
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-05 22:41:12 -0500

    Import CSS into Playlist component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,10 +1,12 @@
 import React from "react";
+// import { TrackList } from "./Tracklist/Tracklist.js";
+import "./Playlist.css";
 
 export class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
-        <input value="New Playlist" />
+        <input defaultValue={"New Playlist"} />
         {/* <!-- Add a TrackList component --> */}
         <button className="Playlist-save">Save this playlist</button>
       </div>
