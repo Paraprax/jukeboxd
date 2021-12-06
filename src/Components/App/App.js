648d5c527d94a4e073208f7cecd49e90004f8383
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-05 21:00:09 -0500

    Write import statements for planned Componets

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import logo from "../../logo.svg";
 import "./App.css";
+import { SearchBar } from "./SearchBar.js";
+import { SearchResults } from "./SearchResults.js";
+import { Playlist } from "./Playlist.js";
 
 export class App extends React.Component {
   render() {
