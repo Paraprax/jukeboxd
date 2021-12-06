40920e116333ba795680aac4f38ef5cb99fbd595
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-06 00:12:52 -0500

    Pass the search results through TrackList to Track as props

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,9 +1,17 @@
 import React from "react";
-import {Track} from '../Track/Track.js';
-import './TrackList.css';
+import { Track } from "../Track/Track.js";
+import "./TrackList.css";
 
+//this now has search results as a prop called 'tracks'
 export class TrackList extends React.Component {
   render() {
-    return <div className="TrackList">{/* <!-- render Track components here(via array.map? We'll see....)  --> */}</div>;
+    return (
+      <div className="TrackList">
+        {this.props.tracks.map((track) => {
+          //mapping the results to an array and passing to the Track component as props
+          return <Track track={track} key={track.id} />;
+        })}
+      </div>
+    );
   }
 }
