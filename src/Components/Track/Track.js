821b4022f1ac09bb22ebeaee471ddb9f17c9abf9
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-05 23:24:30 -0500

    Create Track component

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class Track extends React.Component {
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          {/* <h3><!-- track name here --></h3> */}
+          {/* <p><!-- track artist here--> | <!-- track album here --></p> */}
+        </div>
+        {/* <button className="Track-action"><!-- + or - here --></button> */}
+      </div>
+    );
+  }
+}
