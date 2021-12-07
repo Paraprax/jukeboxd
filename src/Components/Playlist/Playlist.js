cc4d423fc9886b1ecea1c3180e095d8be4639f48
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-06 23:49:15 -0500

    Attach rename handler to onChange attr on input element

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -10,7 +10,7 @@ export class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={"New Playlist"} />
+        <input onChange={this.handleRename} defaultValue={"New Playlist"} />
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
         <button className="Playlist-save">Save this playlist</button>
       </div>
