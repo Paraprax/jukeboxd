874f9e4e3ded487b2120d9850eb65c34e6c0f270
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-06 19:24:17 -0500

    Removed node_modules folder

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -32,7 +32,7 @@ export class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <Playlist playlistTracks={this.state.playlistTracks}/>
+            <Playlist playistname={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { TrackList } from "./Tracklist/Tracklist.js";
+import { TrackList } from "../TrackList/TrackList.js";
 import "./Playlist.css";
 
 export class Playlist extends React.Component {
@@ -7,7 +7,7 @@ export class Playlist extends React.Component {
     return (
       <div className="Playlist">
         <input defaultValue={"New Playlist"} />
-        {/* <!-- Add a TrackList component --> */}
+        <TrackList playlistTracks={this.props.playlistTracks} />
         <button className="Playlist-save">Save this playlist</button>
       </div>
     );
