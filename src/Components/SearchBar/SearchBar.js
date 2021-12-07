4f813893e6d9844a8a2f8da5c9ecd5b18c461509
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-07 00:10:01 -0500

    Add search method to SearchBar.js && pass in state of input param

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./SearchBar.css";
 
 export class SearchBar extends React.Component {
+  search() {
+    this.props.onSearch(this.state.input);
+  }
+
   render() {
     return (
       <div className="SearchBar">
