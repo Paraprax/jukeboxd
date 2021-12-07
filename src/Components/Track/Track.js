b08d00d467d6cd4f5e52a4e9f1a7982559b057ca
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-06 23:34:12 -0500

    Add removeTrack method to Track component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -5,6 +5,7 @@ export class Track extends React.Component {
   constructor(props) {
     super(props);
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   renderAction() {
@@ -21,6 +22,10 @@ export class Track extends React.Component {
 
   addTrack() {
     this.props.onAdd(this.props.track);
+  }
+
+  removeTrack() {
+    this.props.onRemove(this.props.track);
   }
 
   render() {
