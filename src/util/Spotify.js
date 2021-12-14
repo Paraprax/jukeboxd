bebf664e170527aaea34790ff7e89057dac29f0e
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-13 23:22:59 -0500

    Refactor then() chain to fix nesting/bracket issue

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -52,28 +52,34 @@ export const Spotify = {
         }));
       });
   },
+
   savePlaylist(listName, trackURIArray) {
     //breakout if args are empty:
     if (!listName || !trackURIArray.length) {
       return;
     }
+
     //otherwise get the user's username via authorized request:
     const token = this.getAccessToken();
     const headers = {
       Authorization: `Bearer:  ${token}`,
     };
     let userId;
-    return fetch(`https://api.spotify.com/v1/me`, {
-      headers: headers,
-    })
+
+    return fetch(`https://api.spotify.com/v1/me`, { headers: headers })
       .then((response) => response.json())
       .then((jsonResponse) => {
         userId = jsonResponse.id;
         return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-            headers: headers,
-            method: 'POST',
-            body: JSON.stringify({name: listName})
-        });
+          header: headers,
+          method: "POST",
+          body: JSON.stringify({ name: listName }),
+        })
+          .then((response) => response.json())
+          .then((jsonResponse) => {
+            const newPlaylistId = jsonResponse.id;
+          });
+      });
   },
 };
 
