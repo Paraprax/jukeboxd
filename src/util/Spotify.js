36dc69f04fe7cad9e668caea19545d62fadd0f3a
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-12-16 17:54:11 -0500

    Debugging, console.logs

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -20,6 +20,7 @@ const Spotify = {
       //clear vars && grab new token on expiry:
       window.setTimeout(() => (userAccessToken = ""), expiresIn * 1000);
       window.history.pushState("Access Token", null, "/");
+      console.log("valid access token returned");
       return userAccessToken;
     } else {
       const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
@@ -28,7 +29,7 @@ const Spotify = {
   },
 
   searchSpotify(searchTerm) {
-    const token = this.getAccessToken();
+    const token = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
       headers: {
         Authorization: `Bearer:  ${token}`,
